4b8ea580c4b64712a13927f05513dd6418f16811
אורי ליאור < >
2017-03-22 09:41:21 +0200

    Initial commit

---
+++ app/src/main/java/lior/uri/lec2/Lec2Activity.java
@@ -0,0 +1,13 @@
+package lior.uri.lec2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lec2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lec2);
+    }
+}
